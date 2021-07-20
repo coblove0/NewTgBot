56bfdfea8e8a8f746d1e3a61d608853fa72c3e43
coblove0 <coblove0@gmail.com>
2021-07-20 18:31:00 +0400

    format

--- ChatVM.cs
+++ ChatVM.cs
@@ -13,72 +13,72 @@ using Newtonsoft.Json;
 
 namespace NewTgBot
 {
-    public class ChatVM : INotifyPropertyChanged//, ICommand
+  public class ChatVM : INotifyPropertyChanged//, ICommand
+  {
+
+    private TelegramBotClient bot;
+
+    Serializator serializator = new Serializator();
+
+    public long UserId { get; set; }
+
+    public string Name { get; set; }
+    public ObservableCollection<MessageModel> Messages { get; } = new ObservableCollection<MessageModel>();
+    [JsonIgnore]
+    public Command SendMessage { get; }
+
+    private string textMessage;
+    [JsonIgnore]
+    public string TextMessage
     {
+      get
+      {
+        return this.textMessage;
+      }
 
-        private TelegramBotClient bot;
-
-        Serializator serializator = new Serializator();
-
-        public long UserId { get; set; }
-
-        public string Name { get; set; }
-        public ObservableCollection<MessageModel> Messages { get; } = new ObservableCollection<MessageModel>();
-        [JsonIgnore]
-        public Command SendMessage { get; }
-
-        private string textMessage;
-        [JsonIgnore]
-        public string TextMessage
-        {
-            get
-            {
-                return this.textMessage;
-            }
-
-            set
-            {
-                this.textMessage = value;
-                OnPropertyChanged("TextMessage"); //Если свойство меняется, вызывается метод, который уведомляет об изменении модели
-            }
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-        public void OnPropertyChanged([CallerMemberName] string prop = "")
-        {
-            if (PropertyChanged != null)
-            {
-                PropertyChanged(this, new PropertyChangedEventArgs(prop));
-            }
-        }
-
-        public ChatVM(long userId, string name, TelegramBotClient bot)
-        {
-            UserId = userId;
-            Name = name;
-            this.SendMessage = new Command(this.CanExecute, this.Execute);
-            this.bot = bot;
-        }
-
-        public override string ToString()
-        {
-            return this.Name;
-        }
-
-        public void Execute(object parameter)
-        {
-            bot.SendTextMessageAsync(UserId, TextMessage);
-            var message = new MessageModel(DateTime.Now.ToString(), UserId, "bot", textMessage);
-            Messages.Add(message);
-            
-            //serializator.Selialize(ChatListVM.Chats);
-            TextMessage = "";
-        }
-
-        public bool CanExecute(object parameter)
-        {
-            return !string.IsNullOrWhiteSpace(textMessage);
-            //return true;
-        }
+      set
+      {
+        this.textMessage = value;
+        OnPropertyChanged("TextMessage"); //Если свойство меняется, вызывается метод, который уведомляет об изменении модели
+      }
     }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    public void OnPropertyChanged([CallerMemberName] string prop = "")
+    {
+      if (PropertyChanged != null)
+      {
+        PropertyChanged(this, new PropertyChangedEventArgs(prop));
+      }
+    }
+
+    public ChatVM(long userId, string name, TelegramBotClient bot)
+    {
+      UserId = userId;
+      Name = name;
+      this.SendMessage = new Command(this.CanExecute, this.Execute);
+      this.bot = bot;
+    }
+
+    public override string ToString()
+    {
+      return this.Name;
+    }
+
+    public void Execute(object parameter)
+    {
+      bot.SendTextMessageAsync(UserId, TextMessage);
+      var message = new MessageModel(DateTime.Now.ToString(), UserId, "bot", textMessage);
+      Messages.Add(message);
+
+      //serializator.Selialize(ChatListVM.Chats);
+      TextMessage = "";
+    }
+
+    public bool CanExecute(object parameter)
+    {
+      return !string.IsNullOrWhiteSpace(textMessage);
+      //return true;
+    }
+  }
 }
--- MessageModel.cs
+++ MessageModel.cs
@@ -9,38 +9,38 @@ using System.Windows.Input;
 
 namespace NewTgBot
 {
-    public class MessageModel : INotifyPropertyChanged
+  public class MessageModel : INotifyPropertyChanged
+  {
+    private string time;
+    public string Time
     {
-        private string time;
-        public string Time
-        {
-            get { return time; }
-        }
-
-        private long id;
-        public long Id { get { return id; } }
-
-        private string firstName;
-        public string FirstName { get { return firstName; } }
-
-        private string msg;
-        public string Msg { get { return msg; } }
-
-        public MessageModel(string time, long id, string firstName, string msg)
-        {
-            this.time = time;
-            this.id = id;
-            this.firstName = firstName;
-            this.msg = msg;
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-        public void OnPropertyChanged([CallerMemberName]string prop = "")
-        {
-            if (PropertyChanged != null)
-            {
-                PropertyChanged(this, new PropertyChangedEventArgs(prop));
-            }
-        }
+      get { return time; }
     }
+
+    private long id;
+    public long Id { get { return id; } }
+
+    private string firstName;
+    public string FirstName { get { return firstName; } }
+
+    private string msg;
+    public string Msg { get { return msg; } }
+
+    public MessageModel(string time, long id, string firstName, string msg)
+    {
+      this.time = time;
+      this.id = id;
+      this.firstName = firstName;
+      this.msg = msg;
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    public void OnPropertyChanged([CallerMemberName] string prop = "")
+    {
+      if (PropertyChanged != null)
+      {
+        PropertyChanged(this, new PropertyChangedEventArgs(prop));
+      }
+    }
+  }
 }
